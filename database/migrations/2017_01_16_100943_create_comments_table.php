41eba5dee5524a7e25baf21a3245815dc4f4c782
Percy MAMEDY <percy.mamedy@businessdecision.com>
2017-01-17 09:41:14 +0400

    Initial Commit

---
+++ database/migrations/2017_01_16_100943_create_comments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function (Blueprint $table) {
+			$table->increments('id');
+			$table->unsignedInteger('post_id');
+			$table->string('comment');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+		Schema::table('comments', function (Blueprint $table) {
+			$table->foreign('post_id')
+			      ->references('id')
+			      ->on('posts');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('comments', function (Blueprint $table) {
+			$table->dropForeign('comments_post_id_foreign');
+		});
+
+		Schema::dropIfExists('comments');
+	}
+}
